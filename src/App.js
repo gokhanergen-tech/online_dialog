f35aa6c54d4bef06cbd23e4862f9d08a7567a484
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-04-04 21:13:37 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-       <p>Hellofdfsd fsdfsd f dsf sdfsd fsdf</p>
+      <p>dsfsdfsdf</p>
     </div>
   );
 }
