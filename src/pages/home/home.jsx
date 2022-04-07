83c0dcbfc9a1fab7d62e6db8119cdff66b36b575
gokhanergen-tech <ergen5171@gmail.com>
2022-04-07 23:56:02 +0300

    service_item

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Button from '../../components/button/button'
 import Content from '../../components/content_base/content'
-
+import ServiceItem from '../../components/service_item/service_item'
 const Home = () => {
   return (
     <Content>
        <div>
-         <Button value="Sign up"></Button>
+         
        </div>
     </Content>
   )
