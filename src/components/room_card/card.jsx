bc6145f0f0ecc93383ffe324b3e0cd3a1f952ec6
gokhanergen-tech <ergen5171@gmail.com>
2022-04-26 17:50:55 +0300

    card_update

--- src/components/room_card/card.jsx
+++ src/components/room_card/card.jsx
@@ -6,27 +6,26 @@ import styles from './card.module.css'
 const Card = ({ roomName, title, teacher, id }) => {
   return (
     <div className="p-3 col-xl-4 col-md-6 p-4 mt-3">
-     <div className="card bg-white">
+     <div className={styles.card}>
       <div className="first  text-center">
-        <div className="heading text-black fs-4" style={{ fontWeight: "bold" }}>
+        <div className={"heading text-white fs-4 "+ styles.title }>
           {roomName}
         </div>
-        <div className=" text-black fs-5 "> {title}</div>
+        <div className=" fs-5 "> {title}</div>
       </div>
       <div className="second d-flex flex-row mt-2">
         <div className="image mr-3 border border-light rounded-circle">
           <img src="/images/office.png" className="rounded-circle" width="60" />
         </div>
-        <div className=" align-self-center fs-5 text-black m-3 ml-1">
+        <div className=" align-self-center fs-5 m-3 ml-1">
           {teacher}
         </div>
       </div>
 
-      <div className="third mt-4 align-self-center">
+      <div className="d-flex justify-content-center mt-4 align-self-center">
         <Link to={`/interview/${id}`}>
           <button
-            className="btn btn-block text-white"
-            style={{ backgroundColor: "#524E4E" }}
+            className={'btn text-white border border-2 border-light' + styles.button}
           >
             Join The Room
           </button>
