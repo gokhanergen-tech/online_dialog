98e2814b91dd4c9b0869e978fe6dab96a95f497c
gokhanergen-tech <ergen5171@gmail.com>
2022-06-11 12:34:40 +0300

    webRTC_connections

--- socket_pages/room_page.js
+++ socket_pages/room_page.js
@@ -1,138 +1,172 @@
 const { validateRoom } = require("../http/axiosRequest")
-const {ROOM_ACTIONS} = require("../socket/socket-actions")
+const { ROOM_ACTIONS } = require("../socket/socket-actions")
 
 
-let socketMapToUser={}
-let roomToMapMessages={}
+let socketMapToUser = {}
+let roomToMapMessages = {}
 
-const roomPage=(socket,io)=>{
- 
-    const leave=(roomId)=>{
-       delete socketMapToUser[socket.id]
-       io.to(roomId).emit(ROOM_ACTIONS.LEAVE,{user:socket.userData.user})
+const roomPage = (socket, io) => {
+
+    const leave = (roomId) => {
+        io.to(roomId).emit(ROOM_ACTIONS.LEAVE, { user: socket.userData.user })
     }
 
-    const findUser=(email)=>{
-        const keys=Object.keys(socketMapToUser);
-        for(var i=keys.length-1;i>=0;i--){
-            if(socketMapToUser[keys[i]].user.email===email)
-               return keys[i];
+    const initPermissions = () => {
+        if (socket.userData.user.userDto.owner)
+            socket.isCameraShare = true;
+        else {
+            socket.isCameraShare = false;
+        }
+
+    }
+
+    const findUser = async (email) => {
+        const sockets = await io.in(socket.roomId).fetchSockets();
+        for (var i = sockets.length - 1; i >= 0; i--) {
+            if (sockets[i].userData.user.email === email)
+                return sockets[i];
         }
         return false
     }
 
-    const hasRoom=(roomId)=>{
-      return socket.rooms.has(roomId)
+    const hasRoom = (roomId) => {
+        return socket.rooms.has(roomId)
     }
 
-    socket.on("disconnecting",(reason)=>{
-        
-        if(reason==="ping timeout" || reason==="transport close"){
-          socket.rooms.forEach((roomId)=>{
-                socket.broadcast.to(roomId).emit(ROOM_ACTIONS.LEAVE,{user:socket.userData.user})
-          })
-          delete socketMapToUser[socket.id]
+    socket.on("disconnecting", (reason) => {
+
+        if (reason === "ping timeout" || reason === "transport close") {
+            socket.rooms.forEach((roomId) => {
+                socket.broadcast.to(roomId).emit(ROOM_ACTIONS.LEAVE, { user: socket.userData.user })
+            })
         }
-        
+
     })
 
-    socket.on(ROOM_ACTIONS.JOIN,async ({roomId})=>{
- 
-        try{
-            if(roomId){
-                const {data}=await validateRoom(roomId,socket.accessToken);
-               
-                const user = socket.userData.user;
-                const rooms=Object.keys(socketMapToUser)
-                .filter(userRoom=>socketMapToUser[userRoom].user.email===user.email).map(userRoom=>({hashedId:socketMapToUser[userRoom].room.hashedId,socket:userRoom}));
-                
-                if(data.isOpen && !rooms.map(room=>room.hashedId).includes(roomId)){
-                    if(io.to(roomId)){
-                        
-                        io.to(roomId).emit(ROOM_ACTIONS.JOIN,{user})
-                        const sockets=await io.in(roomId).fetchSockets();
-                        sockets.forEach(socketUser=>{
-                            if(socketMapToUser[socketUser.id])
-                             socket.emit(ROOM_ACTIONS.JOIN,{user:socketMapToUser[socketUser.id].user})
-                        })
-                    }
-                    const room=data.room;
+    socket.on(ROOM_ACTIONS.JOIN, async ({ roomId }) => {
 
-                    if(room.roomTypeDto.roomType==="INTERVIEW_ROOM"){
-                        if(!roomToMapMessages[roomId])
-                            roomToMapMessages[roomId]=[]
+        try {
+            if (roomId) {
+                const { data } = await validateRoom(roomId, socket.accessToken);
+
+                const sockets = await io.in(roomId).fetchSockets();
+                const user = socket.userData.user;
+                const rooms = sockets
+                    .filter(userRoom => userRoom.userData.user.email === user.email).map(userRoom => userRoom.roomId);
+
+                if (data.isOpen && !rooms.includes(roomId)) {
+                    if (io.to(roomId)) {
+
+                        io.to(roomId).emit(ROOM_ACTIONS.JOIN, { user })
+
+                        sockets.forEach(socketUser => {
+                            socket.emit(ROOM_ACTIONS.JOIN, { user: socketUser.userData.user })
+                        })
+                        socket.emit(ROOM_ACTIONS.JOIN, { user })
                     }
-                    socketMapToUser[socket.id]={user,room};
-                    socket.roomId=roomId;
+                    const room = data.room;
+
+                    if (room.roomTypeDto.roomType === "INTERVIEW_ROOM") {
+                        if (!roomToMapMessages[roomId])
+                            roomToMapMessages[roomId] = []
+                    }
+                    initPermissions();
+                    socket.roomId = roomId;
                     socket.join(roomId);
-                    socket.emit(ROOM_ACTIONS.ON_JOIN,{})
-                }else{
+                    socket.emit(ROOM_ACTIONS.ON_JOIN, {})
+                } else {
                     socket.disconnect();
                 }
-           } 
-        }catch(err){
-          leave();
-          socket.disconnect();
+            }
+        } catch (err) {
+            leave();
+            socket.disconnect();
         }
     })
 
-    socket.on(ROOM_ACTIONS.LEAVE,()=>{
-         leave(socket.roomId)
-         socket.emit(ROOM_ACTIONS.ON_LEAVE,{})  
+    socket.on(ROOM_ACTIONS.LEAVE, () => {
+        leave(socket.roomId)
+        socket.emit(ROOM_ACTIONS.ON_LEAVE, {})
     })
 
-    socket.on(ROOM_ACTIONS.SEND_MESSAGE,({toUser,message})=>{
-        const authUser=socket.userData;
+    socket.on(ROOM_ACTIONS.SEND_MESSAGE, async ({ toUser, message }) => {
+        const authUser = socket.userData;
 
-        if(socket.roomId){
-            const roomId=socket.roomId;
-            const currentDate=Date.now()
-           
-            const editedMessage={
-                user:authUser.user,
-                message:message,
-                messageDate:currentDate
+        if (socket.roomId) {
+            const roomId = socket.roomId;
+            const currentDate = Date.now()
+
+            const editedMessage = {
+                user: authUser.user,
+                message: message,
+                messageDate: currentDate
             };
 
-            if(!toUser){ 
-                if(hasRoom(roomId)){
-                    io.to(roomId).emit(ROOM_ACTIONS.ON_MESSAGE,{messageUser:editedMessage,selectedMessages:"0"})
+            if (!toUser) {
+                if (hasRoom(roomId)) {
+                    io.to(roomId).emit(ROOM_ACTIONS.ON_MESSAGE, { messageUser: editedMessage, selectedMessages: "0" })
                 }
-            }else{
-                const socketUser=findUser(toUser)
-                if(socketUser){
-                   
-                    if(!authUser?.user?.userDto?.owner){
-                        if(socketMapToUser[socketUser].user.userDto.owner){
-                              //Send to room owner
-                             
-                              console.log(socketMapToUser[socketUser].user.userDto.owner)
-                              io.to(socketUser).emit(ROOM_ACTIONS.ON_MESSAGE,{messageUser:editedMessage,selectedMessages:authUser.user.email})
-                              socket.emit(ROOM_ACTIONS.ON_MESSAGE,{messageUser:editedMessage,selectedMessages:toUser})
-                        }else{
-                              //send error message
+            } else {
+                const socketUser = await findUser(toUser)
+                if (socketUser) {
+
+                    if (!authUser?.user?.userDto?.owner) {
+                        if (socketUser.userData.user.userDto.owner) {
+                            //Send to room owner
+
+                            console.log(socketUser.userData.user.userDto.owner)
+                            socketUser.emit(ROOM_ACTIONS.ON_MESSAGE, { messageUser: editedMessage, selectedMessages: authUser.user.email })
+                            socket.emit(ROOM_ACTIONS.ON_MESSAGE, { messageUser: editedMessage, selectedMessages: toUser })
+                        } else {
+                            //send error message
                         }
-                    }else{
-                        if(!socketMapToUser[socketUser].user.userDto.owner){
-                                //Send to room owner
-                            io.to(socketUser).emit(ROOM_ACTIONS.ON_MESSAGE,{messageUser:editedMessage,selectedMessages:authUser.user.email})
-                            socket.emit(ROOM_ACTIONS.ON_MESSAGE,{messageUser:editedMessage,selectedMessages:toUser})
-                        }else{
-                                //send error message
-                        } 
+                    } else {
+                        if (!socketUser.userData.user.userDto.owner) {
+                            //Send to room owner
+                            socketUser.emit(ROOM_ACTIONS.ON_MESSAGE, { messageUser: editedMessage, selectedMessages: authUser.user.email })
+                            socket.emit(ROOM_ACTIONS.ON_MESSAGE, { messageUser: editedMessage, selectedMessages: toUser })
+                        } else {
+                            //send error message
+                        }
                     }
-                }    
+                }
             }
         }
-       
+
 
     })
 
+    socket.on(ROOM_ACTIONS.RELAY_ICE, async ({ email, icecandidate }) => {
+        const socketUser = await findUser(email)
+        if (socketUser)
+            socketUser.emit(ROOM_ACTIONS.ICE_CANDIDATE, {
+                icecandidate,email:socket.userData.user.email
+            })
+    })
 
-    
+    socket.on(ROOM_ACTIONS.SEND_OFFER, async ({ email, offer }) => {
+        const socketUser = await findUser(email)
+        if (socketUser)
+            socketUser.emit(ROOM_ACTIONS.SESSION_DESCRIPTION, {
+                offer,email:socket.userData.user.email
+            })
+    })
+
+    socket.on(ROOM_ACTIONS.PERMISSON_CONTROL,({type})=>{
+         switch(type){
+            case "VIDEO":
+                if(socket.isCameraShare){
+                   socket.emit(ROOM_ACTIONS.ACCEPT_VIDEO_STREAM,{})
+                }
+                break;
+         }
+    })
+
+
+
 
 }
 
-module.exports=roomPage;
+module.exports = roomPage;
 
 
